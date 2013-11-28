bf2625be9a90232e0655aab64ea49afdd7b18647
idknox <knoxid@gmail.com>
2013-11-27 18:37:15 -0700

    Create Euler16.py

---
+++ Euler16.py
@@ -0,0 +1,17 @@
+'''
+215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 21000?
+'''
+
+num= 2**1000
+numlist=[]
+print num
+string = str(num)
+print string
+lst = list(string)
+print lst
+for each in lst:
+    each = int(each)
+    numlist.append(each)
+print sum(numlist)
